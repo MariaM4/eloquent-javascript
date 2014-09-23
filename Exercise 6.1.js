ec6b229ef1bfb3dab17f64a99c1779f082a58aa6
mary <mmaguzo@gmail.com>
2014-09-23 15:26:55 +0300

    Added the codes for exercise 6.1

---
+++ Exercise 6.1.js
@@ -0,0 +1,21 @@
+function countZeroes(array) {
+  function counter(total, element) {
+    return total + (element === 0 ? 1 : 0);
+  }
+  return reduce(counter, 0, array);
+}
+
+
+function count(test, array) {
+  return reduce(function(total, element) {
+    return total + (test(element) ? 1 : 0);
+  }, 0, array);
+}
+
+function equals(x) {
+  return function(element) {return x === element;};
+}
+
+function countZeroes(array) {
+  return count(equals(0), array);
+}
