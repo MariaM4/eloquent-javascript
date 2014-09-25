86d690e37644f895a391e58039536d962583f07d
mary <mmaguzo@gmail.com>
2014-09-25 16:01:57 +0300

    Added the codes for exercise 7.4

---
+++ Exercise 7.4.js
@@ -0,0 +1,31 @@
+function shortestRoute(from, to) {
+  var currentShortest = null;
+  forEach(possibleRoutes(from, to), function(route) {
+    if (!currentShortest || currentShortest.length > route.length)
+      currentShortest = route;
+  });
+  return currentShortest;
+}
+
+function minimise(func, array) {
+  var minScore = null;
+  var found = null;
+  forEach(array, function(element) {
+    var score = func(element);
+    if (minScore == null || score < minScore) {
+      minScore = score;
+      found = element;
+    }
+  });
+  return found;
+}
+
+function getProperty(propName) {
+  return function(object) {
+    return object[propName];
+  };
+}
+
+function shortestRoute(from, to) {
+  return minimise(getProperty("length"), possibleRoutes(from, to));
+}
