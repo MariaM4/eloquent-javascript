62893009ac9079d63283b9b488d14ff63f075ed9
mary <mmaguzo@gmail.com>
2014-10-01 10:52:06 +0300

    Added part two of the codes for exercise 10.4

--- Exercise 10.4.js
+++ Exercise 10.4.js
@@ -6,3 +6,13 @@ function escapeHTML(text) {
   });
   return text;
 }
+
+function escapeHTML(text) {
+  var replacements = {"<": "&lt;", ">": "&gt;",
+                      "&": "&amp;", "\"": "&quot;"};
+  return text.replace(/[<>&"]/g, function(character) {
+    return replacements[character];
+  });
+}
+
+console.log(escapeHTML("The 'pre-formatted' tag is written \"<pre>\"."));
